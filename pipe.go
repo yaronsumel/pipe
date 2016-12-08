ec371b8abf4c883b2a8048d65f050d4946edc3fb
Yaron Sumel <yaronsu@gmail.com>
2016-12-08 17:08:51 +0800

    add pipe.go

---
+++ pipe.go
@@ -0,0 +1,80 @@
+package pipe
+
+import (
+	"os"
+	"bufio"
+	"io"
+	"errors"
+)
+
+// used here to prevent os package import
+var (
+	Stdin = os.Stdin
+	Stdout = os.Stdout
+	Stderr = os.Stderr
+)
+
+// StdData used for as async communication channel
+type StdDataChannel chan StdData
+type StdData struct {
+	Data []byte
+	Err  error
+}
+
+// isNamedPipe checks whether valid pipe used
+func isNamedPipe() bool {
+	fileInfo, err := os.Stdin.Stat()
+	if err != nil {
+		return false
+	}
+	return ((fileInfo.Mode() & os.ModeNamedPipe) == os.ModeNamedPipe)
+}
+
+// readChunk read from io.reader amount of bytes defined in size
+// returns error if something is wrong or []byte if its fine
+func readChunk(r io.Reader, size int) ([]byte, error) {
+	buf := make([]byte, 0, size)
+	n, err := r.Read(buf[:cap(buf)])
+	if err != nil {
+		return []byte{}, err
+	}
+	if n == 0 {
+		if err == nil {
+			return []byte{}, err
+		}
+		if err == io.EOF {
+			return []byte{}, err
+		}
+	}
+	buf = buf[:n]
+	return buf, nil
+}
+
+// Read reads from io.reader one chunk as defined as bufSize
+func Read(pipe io.Reader, bufSize int) ([]byte, error) {
+	if !isNamedPipe() {
+		return []byte{}, errors.New("Not Valid Named Pipe")
+	}
+	return readChunk(bufio.NewReader(pipe), bufSize)
+}
+
+// AsyncRead keeps reading from io.reader into buffer
+// return data in stdData structure
+// use in only with routine - ie go AsyncRead
+func AsyncRead(pipe io.Reader, bufSize int, stdDataChan chan StdData) {
+	if !isNamedPipe() {
+		stdDataChan <- StdData{
+			Err:errors.New("Not Valid Named Pipe"),
+		}
+	}
+	r := bufio.NewReader(pipe)
+	for {
+		stdData := StdData{}
+		data, err := readChunk(r, bufSize)
+		if err != nil {
+			stdData.Err = err
+		}
+		stdData.Data = data
+		stdDataChan <- stdData
+	}
+}
